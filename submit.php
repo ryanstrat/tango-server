12ccd519834b2bd8e8afef6c127471e4df19f521
Ryan Strat <ryanstrat@users.noreply.github.com>
2015-01-17 16:29:43 -0500

    add var_dump for

--- submit.php
+++ submit.php
@@ -2,6 +2,8 @@
 
 
 openlog("myScriptLog", LOG_PID | LOG_PERROR, LOG_LOCAL0);
-syslog (LOG_DEBUG , "PHP: ".var_dump($_REQUEST));
+syslog (LOG_DEBUG , "PHP Request: ".var_dump($_REQUEST));
+syslog (LOG_DEBUG , "PHP Post: ".var_dump($_POST));
+
 
 closelog();
