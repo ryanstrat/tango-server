5b136e14cf4449e9f7a4b4afee96d68f091b2103
Ryan Strat <ryanstrat@users.noreply.github.com>
2015-01-18 12:00:28 -0500

    revert adding id

--- query.php
+++ query.php
@@ -9,14 +9,14 @@ $hostname = "192.168.1.51";
 $mysqli = new mysqli($hostname, $username, $password, "mhacksv");
 
 if(!empty($_GET)) {
-    if($result = $mysqli->query("SELECT xyz_count, xyz_parcel, timestamp, id FROM tango WHERE id=".$_GET['id'])) {
+    if($result = $mysqli->query("SELECT xyz_count, xyz_parcel, timestamp FROM tango WHERE id=".$_GET['id'])) {
         $obj = $result->fetch_object();
         echo json_encode($obj);
     } else {
         echo $mysqli->error;
     }
 } else {
-    if($result = $mysqli->query("SELECT xyz_count, xyz_parcel, timestamp, id FROM tango ORDER BY id DESC LIMIT 1;")) {
+    if($result = $mysqli->query("SELECT xyz_count, xyz_parcel, timestamp FROM tango ORDER BY id DESC LIMIT 1;")) {
         $obj = $result->fetch_object();
         echo json_encode($obj);
     }
