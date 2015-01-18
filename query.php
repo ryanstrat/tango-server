2ffe9b22de66d488b7b22bccb529be26b2b7ddd2
Ryan Strat <ryanstrat@users.noreply.github.com>
2015-01-18 02:17:00 -0500

    edit sql sort

--- query.php
+++ query.php
@@ -9,7 +9,7 @@ $hostname = "192.168.1.51";
 $mysqli = new mysqli($hostname, $username, $password, "mhacksv");
 
 
-if($result = $mysqli->query("SELECT xyz_count, xyz_parcel, timestamp FROM tango ORDER BY timestamp DESC LIMIT 1;")) {
+if($result = $mysqli->query("SELECT xyz_count, xyz_parcel, timestamp FROM tango ORDER BY id DESC LIMIT 1;")) {
     $obj = $result->fetch_object();
     echo json_encode($obj);
 }
