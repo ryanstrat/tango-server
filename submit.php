260b07179f1cf04de2e377eb41a820fc5b07a9a3
Ryan Strat <ryanstrat@users.noreply.github.com>
2015-01-17 20:07:11 -0500

    Add more fields to DB

--- submit.php
+++ submit.php
@@ -11,7 +11,12 @@ syslog(LOG_DEBUG, "Post0: ".$_POST['timestamp']);
 
 $conn = new mysqli($hostname, $username, $password, "mhacksv");
 
-if (!$conn->query("INSERT INTO tango (timestamp) VALUES (".$_POST['timestamp'].")")) {
+$timestamp = $_POST['timestamp'];
+$ij_cols = $_POST['ij_cols'];
+$ij_rows = $_POST['ij_rows'];
+$xyz_count = $_POST['xyz_count'];
+
+if (!$conn->query("INSERT INTO tango (ij_cols, ij_rows, xyz_count, timestamp) VALUES (".$ij_cols.",".$ij_rows.","$xyz_count.","$timestamp.")")) {
     syslog(LOG_DEBUG, "Data insert failed: (" . $conn->errno . ") " . $conn->error);
 }
 
