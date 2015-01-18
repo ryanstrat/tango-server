92c926c1d4b41fa85ecd1ea87c0e5b01db0545d5
Ryan Strat <ryanstrat@users.noreply.github.com>
2015-01-18 10:40:01 -0500

    id

--- query.php
+++ query.php
@@ -8,7 +8,7 @@ $hostname = "192.168.1.51";
 
 $mysqli = new mysqli($hostname, $username, $password, "mhacksv");
 
-if(!is_null($_GET['q'])) {
+if(!is_null($_GET)) {
     if($result = $mysqli->query("SELECT xyz_count, xyz_parcel, timestamp, id FROM tango WHERE id=".$_GET['q']."ORDER BY id DESC LIMIT 1;")) {
         $obj = $result->fetch_object();
         echo json_encode($obj);
