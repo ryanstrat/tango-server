f7d14e6b46fb9684a035c9bcfd57eb0914371b2e
Ryan Strat <ryanstrat@users.noreply.github.com>
2015-01-18 00:44:35 -0500

    Done with submit.php

--- submit.php
+++ submit.php
@@ -6,27 +6,19 @@ $username = "mhacksv";
 $password = "mhacksv!";
 $hostname = "192.168.1.51";
 
-syslog(LOG_DEBUG, "XYZ_PARCEL: ".$_POST['xyz_parcel']);
-
 $mysqli = new mysqli($hostname, $username, $password, "mhacksv");
 
 $timestamp = $_POST['timestamp'];
-$ij_cols = $_POST['ij_cols'];
-$ij_rows = $_POST['ij_rows'];
 $xyz_count = $_POST['xyz_count'];
 $xyz_parcel = $_POST['xyz_parcel'];
 
-/*if (!$conn->query("INSERT INTO tango (ij_cols, ij_rows, xyz_count, xyz_parcel, timestamp) VALUES (".$ij_cols.",".$ij_rows.",".$xyz_count.",".$xyz_parcel.",".$timestamp.")")) {
-    syslog(LOG_DEBUG, "Data insert failed: (" . $conn->errno . ") " . $conn->error);
-}*/
+
 if (!($stmt = $mysqli->prepare("INSERT INTO tango (xyz_count, xyz_parcel, timestamp) VALUES (?,?,?)"))) {
     echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
 }
 
 /* Prepared statement, stage 2: bind and execute */
 $stmt->bind_param("isd", $xyz_count, $xyz_parcel, $timestamp);
-//$stmt->bind_param("s", $xyz_parcel);
-//$stmt->bind_param("d", $timestamp);
 
 if (!$stmt->execute()) {
     echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
