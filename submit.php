7ee079a043d21764a9db61c422754fe45c3d0f62
Ryan Strat <ryanstrat@users.noreply.github.com>
2015-01-18 00:28:56 -0500

    I remember how php works...

--- submit.php
+++ submit.php
@@ -24,7 +24,7 @@ if (!($stmt = $mysqli->prepare("INSERT INTO tango (xyz_count, xyz_parcel, timest
 }
 
 /* Prepared statement, stage 2: bind and execute */
-$stmt->bind_param("i", $xyz_count, "s", $xyz_parcel, "d", $timestamp);
+$stmt->bind_param("isd", $xyz_count, $xyz_parcel, $timestamp);
 //$stmt->bind_param("s", $xyz_parcel);
 //$stmt->bind_param("d", $timestamp);
 
