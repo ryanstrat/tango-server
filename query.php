6435f7e31241547c2c119c1848e91b124a498b66
Ryan Strat <ryanstrat@users.noreply.github.com>
2015-01-18 01:33:14 -0500

    This commit is so fetch

--- query.php
+++ query.php
@@ -11,8 +11,8 @@ $mysqli = new mysqli($hostname, $username, $password, "mhacksv");
 $stmt = $mysqli->stmt_init();
 $stmt = $mysqli->prepare("SELECT xyz_count, xyz_parcel, timestamp FROM tango ORDER BY timestamp DESC LIMIT 1");
 $stmt->execute();
-$res = $stmt->get_result();
-$row = $res->fetch_assoc();
+$res = $stmt->fetch();
+//$row = $res->fetch_assoc();
 
 //printf("id = %s (%s)\n", $row['id'], gettype($row['id']));
 //printf("label = %s (%s)\n", $row['label'], gettype($row['label']));
