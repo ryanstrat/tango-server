8beab0dc3a35cb8ac1abdfe0df645dc6511c37e7
Ryan Strat <ryanstrat@users.noreply.github.com>
2015-01-18 01:07:09 -0500

    Initial work on query.php

--- query.php
+++ query.php
@@ -1,4 +1,23 @@
 <?php
 
+openlog("myScriptLog", LOG_PID | LOG_PERROR, LOG_LOCAL0);
 
+$username = "mhacksv";
+$password = "mhacksv!";
+$hostname = "192.168.1.51";
 
+$mysqli = new mysqli($hostname, $username, $password, "mhacksv");
+
+$stmt = $mysqli->prepare("SELECT xyz_count, xyz_parcel, timestamp FROM tango ORDER BY timestamp DESC LIMIT 1");
+$stmt->execute();
+$res = $stmt->get_result();
+$row = $res->fetch_assoc();
+
+//printf("id = %s (%s)\n", $row['id'], gettype($row['id']));
+//printf("label = %s (%s)\n", $row['label'], gettype($row['label']));
+
+$stmt->close();
+
+$mysqli->close();
+
+closelog();
